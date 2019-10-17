a219e6d863eed886a3d6426fea2e915e6e8c7872
Miram_Mendez <miriancrid123@gmail.com>
2019-10-17 16:42:52 -0500

    holaa

---
+++ EstadoVariable/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EstadoVariable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnContar = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnContar
+            // 
+            this.btnContar.Location = new System.Drawing.Point(381, 72);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(75, 23);
+            this.btnContar.TabIndex = 0;
+            this.btnContar.Text = "Contar";
+            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(261, 72);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 1;
+            this.txtNumero.TextChanged += new System.EventHandler(this.txtNumero_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.btnContar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnContar;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
+
---
+++ WebApplication1/PrimeroUNO.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PrimeroUNO.aspx.cs" Inherits="WebApplication1.PrimeroUNO" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="Miriam" runat="server" AutoGenerateColumns="false">
+                <columns>
+                
+                  <asp:templatefield headertext="Nombre">
+                    <itemtemplate>
+                      <asp:label id="FirstNameLabel"
+                        Text= '<%# Eval("au_fname") %>'
+                        runat="server"/> 
+                      <asp:label id="LastNameLabel"
+                        Text= '<%# Eval("au_lname") %>'
+                        runat="server"/>
+                    </itemtemplate>
+                  </asp:templatefield>
+                
+        </columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
